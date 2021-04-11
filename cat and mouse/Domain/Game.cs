7acf101b19f5f8b13b0a23d5155093f9b855fddc
yatimUrFU <yatim2002@gmail.com>
2021-04-11 16:14:02 +0500

    Add pictures

--- cat and mouse/Domain/Game.cs
+++ cat and mouse/Domain/Game.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace cat_and_mouse.Domain
 {
+    public enum GameStage //состяния игры
+    {
+        PlayerChoose,
+        MapChoose,
+        Game,
+        EndGame
+    }
     public class Game
     {
         public Player CatHuman;
         public Player MouseHuman;
         public Player CatAI;
         public Player MouseAI;
+        public Tuple<Player> CurrentPlayers;// = new Tuple<Player>();
     }
 
     public class Field : Cell
